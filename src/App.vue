4601cb08a7b14a337d6fbd9e4d500811bd39bd6c
Haruki Okuda <sunny_smile_0v0@yahoo.co.jp>
2020-07-09 20:56:54 +0900

    コンポーネントファイルの読み込み

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,13 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Dial from './components/Dial.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Dial
   }
 }
 </script>

aa7022740200ebfeee4d468857cac86c01abc010
Haruki Okuda <sunny_smile_0v0@yahoo.co.jp>
2020-07-15 17:15:27 +0900

    リセット機能の追加

--- src/components/Buttons/ResetButton.vue
+++ src/components/Buttons/ResetButton.vue
@@ -1,8 +1,12 @@
 <template>
-    <button>Reset</button>
+    <button @click="resetTimer">Reset</button>
 </template>
 <script>
     export default {
-
+        methods: {
+            resetTimer: function () {
+                this.$parent.$parent.selectedTime = 0;
+            }
+        }
     }
 </script>

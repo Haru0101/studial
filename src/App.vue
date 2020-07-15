7000a53568992bdd57b2dcdea6f21acedbeec2a8
Haruki Okuda <sunny_smile_0v0@yahoo.co.jp>
2020-07-15 16:49:33 +0900

    選択したオプションの分数をDialに表示する

--- src/App.vue
+++ src/App.vue
@@ -2,46 +2,44 @@
   <div id="app">
     <Logo v-show="true"></Logo>
     <Dial v-show="false" :selectedTime="selectedTime"></Dial>
-    <SelectOption v-show="true" :optionTime="optionTime"></SelectOption>
-    <Button v-show="false" :buttonName="buttonName"></Button>
+    <SelectOption v-show="true"></SelectOption>
+    <Buttons v-show="false"></Buttons>
   </div>
 </template>
 
 <script>
-import Dial from './components/Dial'
-import Button from './components/Button'
-import SelectOption from './components/SelectOption'
-import Logo from './components/Logo'
+  import Dial from './components/Dial'
+  import Buttons from './components/Buttons'
+  import SelectOption from './components/SelectOption'
+  import Logo from './components/Logo'
 
-export default {
-  name: 'App',
-  components: {
-    Dial,
-    SelectOption,
-    Button,
-    Logo
-  },
-  data() {
-    return {
-      optionTime: 15,
-      buttonName: "Start",
-      selectedTime: 30
+  export default {
+    name: 'App',
+    components: {
+      Dial,
+      SelectOption,
+      Buttons,
+      Logo
+    },
+    data() {
+      return {
+        selectedTime: null
+      }
     }
   }
-}
 </script>
 
 <style>
-body {
-  background-color: #F4F4F8;
-}
+  body {
+    background-color: #F4F4F8;
+  }
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- src/components/SelectOption.vue
+++ src/components/SelectOption.vue
@@ -1,12 +1,28 @@
 <template>
-    <div>
-        {{ optionTime }}min
+    <div class="buttonsWrap">
+        <button v-for="(optionTime, index) in optionTimeArray" :key="index" @click="selectTime(optionTime)">
+            {{ optionTime }} min
+        </button>
     </div>
 </template>
 <script>
-export default {
-    props: {
-        optionTime: Number
+    export default {
+        data() {
+            return {
+                optionTimeArray: [15, 30, 45, 60],
+            }
+        },
+        methods: {
+            selectTime: function (selectedTime) {
+                this.$parent.selectedTime = selectedTime;
+            }
+        }
     }
-}
 </script>
+<style lang="scss" scoped>
+    .buttonsWrap {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+</style>

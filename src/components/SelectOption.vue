3c6d0691f43f0b92336429ff9a1edb8e0fd9d150
Haruki Okuda <sunny_smile_0v0@yahoo.co.jp>
2020-07-19 20:46:41 +0900

    リアクティブに選択したオプションを表示

--- src/components/SelectOption.vue
+++ src/components/SelectOption.vue
@@ -1,16 +1,8 @@
 <template>
     <div class="optionWrap">
         <input type="range" v-model="$parent.min" max="60" min="0">
-        {{ this.min }} min
     </div>
 </template>
-<script>
-    export default {
-        props: {
-            min: Number
-        },
-    }
-</script>
 <style lang="scss" scoped>
     .optionWrap {
         margin: 40px 0;

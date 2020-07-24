0ced85f56230cd0adae00635968cc71fe6c507b8
Haruki Okuda <sunny_smile_0v0@yahoo.co.jp>
2020-07-24 11:09:02 +0900

    Updates

---
+++ precache-manifest.14ceae7859e4789c935c80eb03930680.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8805bef3e022928a64d8",
+    "url": "/css/app.15cfdd3b.css"
+  },
+  {
+    "revision": "7095d1de38df3e55f70df9368a695a78",
+    "url": "/img/logo.7095d1de.png"
+  },
+  {
+    "revision": "82d6968aae2e56fbccd5d9f144a28a58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8805bef3e022928a64d8",
+    "url": "/js/app.23cf84d4.js"
+  },
+  {
+    "revision": "d80e6992c39d5e7c25a9",
+    "url": "/js/chunk-vendors.7b872170.js"
+  },
+  {
+    "revision": "3b30733f0fdc403a4dd1a9d1d62d7ad4",
+    "url": "/manifest.json"
+  }
+]);
